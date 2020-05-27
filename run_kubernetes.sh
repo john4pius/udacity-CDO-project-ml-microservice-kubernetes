536b0f88833f5de21d3d6fa6dbc274e2ca4128eb
John Chukwuma Pius <jpius@Johns-Mac.local>
2020-05-27 01:40:26 +0100

    update run_kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="john4pius/udacityprojectimage"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+kubectl run mlmicroserviceapi\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=mlmicroserviceapi
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlmicroserviceapi 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="john4pius/udacityprojectimage"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag udacityprojectimage $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
